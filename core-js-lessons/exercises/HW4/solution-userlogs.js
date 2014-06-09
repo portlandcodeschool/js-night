caaed2df5b8a344705074e1de9de91290ba45903
Dan Bauer <dsbauer@gmail.com>
2014-06-09 09:44:46 -0700

    Added HW4 solutions

--- core-js-lessons/exercises/HW4/solution-userlogs.js
+++ core-js-lessons/exercises/HW4/solution-userlogs.js
@@ -3,17 +3,18 @@ var makeUser = (function() {
 
 	function writeToLog(str) {
 		sharedLog.push(this.getName() + ':' + str);
+		return true;
 	}
 
 	function makeUser(name,passwd) {
 		var user = {};
 		user.getName = function () {return name};
 		user.validate = function (tryPasswd) {return tryPasswd === passwd}; 
-		user.log = writeToLog;
+		user.record = writeToLog;
 		return user;
 	}
 
-	makeUser.showLog = function(userObj) {
+	makeUser.getLog = function(userObj) {
 		if (!userObj) return sharedLog.join('\n');
 
 		var userName = userObj.getName();
@@ -29,3 +30,14 @@ var makeUser = (function() {
 	return makeUser;
 })();
 
+var clarissa = makeUser('Clarissa','sooperSekret')
+var dan = makeUser('Dan','pAsSwOrD');
+var ben = makeUser('Ben','JackIsTehCute');
+clarissa.record("All your closure are belong to us!");
+dan.validate('pAsSwOrD');
+dan.record("Variables live in the roundy things");
+ben.record("Github for the win!");
+dan.record("Properties live in the squarey things");
+
+makeUser.getLog(dan);
+makeUser.getLog();
