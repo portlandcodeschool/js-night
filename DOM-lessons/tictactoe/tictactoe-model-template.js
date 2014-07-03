611912b5e9189e4e5f72708a92876377bee2d2ca
Dan Bauer <dsbauer@gmail.com>
2014-07-03 11:18:18 -0700

    Revised TTT exercise

---
+++ DOM-lessons/tictactoe/tictactoe-model-template.js
@@ -0,0 +1,10 @@
+var TicTacToeGame = function() { //module returning constructor:
+
+	// Revised HW5 solution here, including...
+
+	function Constructor(endgameFn, gui) {
+		// more code...
+	}
+
+	return Constructor;	// return TTT game constructor
+}();
