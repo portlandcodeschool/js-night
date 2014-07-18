e646e51b514ebf71739f8859676c6f75d36aef0c
Benjamin P. Verble <bvrider@gmail.com>
2014-07-18 14:31:28 -0700

    bb 7 question and answer

---
+++ backbone-lessons/1-backbone/7-npm-style-backbone-question/public/js/views/todo-list-view.js
@@ -0,0 +1,27 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+
+//CHALLENGE: "bring in" the appropriate template for this view
+
+
+var TodoListView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'list-group',
+  initialize: function () {
+    this.collection.on('add', this.render, this);
+  },
+  render: function () {
+
+    var data = [];
+
+    this.collection.models.forEach(function (item) {
+      data.push({title: item.escape('title'), description: item.escape('description') });
+    });
+
+    this.$el.html(myTemplate({todoData:data}));
+  }
+});
+
+module.exports = TodoListView;
