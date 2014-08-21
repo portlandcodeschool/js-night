119fd1235cc245516a6157c2952b73ceb6bef8d7
Benjamin P. Verble <bvrider@gmail.com>
2014-08-21 14:20:51 -0700

    changes

---
+++ backbone-lessons-client-only-NEW/2-jquery-version/js/blah.js
@@ -0,0 +1,25 @@
+var object = {};
+
+_.extend(object, Backbone.Events);
+
+
+object.on("alert", function(input) {
+  $('#some-other-el').append("<h1>" + input.data + '</h1>' );
+});
+
+
+$().click(function () {
+  object.trigger("alert", {data: 'mah data'});
+})
+
+var myView = Backbone.View.extend({
+  method: function (arg) {
+
+  }
+})
+
+var myNewline = '';
+var aNewline = {};
+
+var balh;
+
